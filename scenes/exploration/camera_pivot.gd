9569318a96d8881c4b772a1d6b1bf214ba7f7dd1
sillpct <theodemedeiros@gmail.com>
2026-07-20 14:03:48 +0200

    Update camera_pivot.gd

--- scenes/exploration/camera_pivot.gd
+++ scenes/exploration/camera_pivot.gd
@@ -4,6 +4,10 @@ extends Node3D
 
 @export var follow_speed: float = 8.0
 
+@export var rotation_sensitivity: float = 0.01
+
+var _is_rotating := false
+
 func _process(delta: float) -> void:
 
 	if target == null:
@@ -12,3 +16,13 @@ func _process(delta: float) -> void:
 		target.global_position,
 		follow_speed * delta
 	)
+
+func _unhandled_input(event: InputEvent) -> void:
+
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_RIGHT:
+			_is_rotating = event.pressed
+			get_viewport().set_input_as_handled()
+	elif event is InputEventMouseMotion and _is_rotating:
+		rotate_y(-event.relative.x * rotation_sensitivity)
+		get_viewport().set_input_as_handled()
