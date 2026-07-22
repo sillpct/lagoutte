5fe93bc92a10ccb2949e366d0878ef16482c771f
sillpct <theodemedeiros@gmail.com>
2026-07-22 18:33:16 +0200

    Update player.gd

--- scenes/entities/player.gd
+++ scenes/entities/player.gd
@@ -47,8 +47,9 @@ func check_interaction() -> void:
 	for area in $InteractionArea.get_overlapping_areas():
 		if area.is_in_group("interactable"):
 			if area.has_method("interact"):
-				area.interact(self)
-				return
+				if area.interact(self):
+					return
+				continue
 			current_interactable = area
 			current_dialogue_index = 0
 			waiting_for_choice = false
